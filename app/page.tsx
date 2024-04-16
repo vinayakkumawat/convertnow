8b180cde9346444bd08e0c64a905b504f79eb964
Vinayak Kumawat <iamvinayakkt@gmail.com>
2024-04-16 21:23:21 +0530

    error solved

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,7 @@
 import Droparea from "@/components/Droparea"
 
+const pageDes = "Looking for a reliable file format converter? ConvertNow is your go-to solution. Convert images, videos, and audio files effortlessly, and it won't cost you a thing. Say goodbye to file compatibility issues. ConvertNow is your one-stop destination for file format conversion."
+
 export default function Home() {
   return (
     <div className="space-y-16 pb-8">
@@ -7,7 +9,7 @@ export default function Home() {
       {/* Title & Description */}
       <div className="space-y-6">
         <h1 className="text-3xl md:text-5xl font-bold text-center">Free Unlimited File <span className="underline decoration-primary">Converter</span></h1>
-        <p className="text-foreground/60 text-md md:text-lg text-center md:px-24 xl:px-44 2xl:px-52">Looking for a reliable file format converter? ConvertNow is your go-to solution. Convert images, videos, and audio files effortlessly, and it won't cost you a thing. Say goodbye to file compatibility issues. ConvertNow is your one-stop destination for file format conversion.</p>
+        <p className="text-foreground/60 text-md md:text-lg text-center md:px-24 xl:px-44 2xl:px-52">{pageDes}</p>
       </div>
 
       {/* Dropbox Area */}
